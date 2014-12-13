ce66f0f3aef69a282bd6e44e83cdccf7a60cf9d7
Andy Tang <andy.tang@jumio.com>
2014-12-13 13:29:59 +0100

    implemented the suggestions function

--- app/viewModels/autoCompleteVM.js
+++ app/viewModels/autoCompleteVM.js
@@ -3,7 +3,14 @@
 
   var app = angular.module('imber');
 
-  app.controller('autoCompleteVM', function autoCompleteVM($scope) {
+  app.controller('autoCompleteVM', function autoCompleteVM($scope, $filter) {
+    var genericFilter = $filter('filter');
+    $scope.value = null;
     $scope.options = [];
+    $scope.suggestions = [];
+
+    $scope.suggest = function suggest() {
+      $scope.suggestions = genericFilter($scope.options, $scope.value);
+    };
   });
 }(window.angular));
--- test/unit/app/viewModels/autoCompleteVMSpecs.js
+++ test/unit/app/viewModels/autoCompleteVMSpecs.js
@@ -2,12 +2,27 @@
   'use strict';
 
   describe('autoCompleteVM specs', function autoCompleteVMSpecs() {
+    var testGlobals, $scope;
+
+    beforeEach(module('imber-test'));
+
+    beforeEach(inject(function setupTest(testSetup) {
+      testGlobals = testSetup.setupControllerTest('autoCompleteVM');
+      $scope = testGlobals.$scope;
+    }));
+
     it('should suggest options the moment the user starts typing', function suggestWhileTyping() {
       // given
+      $scope.options = ['hello', 'hellow', 'helelel'];
 
       // when
+      $scope.value = 'hell'
+      $scope.suggest();
 
       // then
+      expect($scope.suggestions).to.include('hello');
+      expect($scope.suggestions).to.include('hellow');
+      expect($scope.suggestions).not.to.include('helelel');
     });
 
     it('should dynamically load the options from the server', function dynamicLoading() {
