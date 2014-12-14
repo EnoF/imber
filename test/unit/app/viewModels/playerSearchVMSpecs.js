99db5d887973e130b57225afa571212c24063191
Andy Tang <andy.tang@jumio.com>
2014-12-14 17:22:51 +0100

    scaffolded the player search specs for the VM and widget

---
+++ test/unit/app/viewModels/playerSearchVMSpecs.js
@@ -0,0 +1,23 @@
+(function playerSearchVMSpecsScope() {
+  'use strict';
+
+  describe('playerSearchVM specs', function playerSearchVMSpecs() {
+    it('should search for a player', function searchPlayer() {
+      // given
+
+      // when
+
+      // then
+
+    });
+
+    it('should provide the selected player model', function selectedPlayerModel() {
+      // given
+
+      // when
+
+      // then
+
+    });
+  });
+}());
---
+++ test/unit/app/widgets/playerSearchWidgetSpecs.js
@@ -0,0 +1,14 @@
+(function playerSearchWidgetSpecsScope(angular) {
+  'use strict';
+
+  describe('<player-search> specs', function playerSearchWidgetSpecs() {
+    it('should emit the selected player', function emitSelectedPlayer() {
+      // given
+
+      // when
+
+      // then
+
+    });
+  });
+}(window.angular));
