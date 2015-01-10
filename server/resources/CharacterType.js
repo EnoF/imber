21bfba441b50c7760fa1b67a9dfecf0207c3d1dd
Andy Tang <andyt@live.nl>
2015-01-11 00:59:56 +0100

    CharacterType Schema is now binding to the correct Collection This was causing a problem for the `mongoose.populate`. The reference would not be found, causing the schema to return null for the reference, even though the record exists.

--- server/resources/CharacterType.js
+++ server/resources/CharacterType.js
@@ -1,23 +1,25 @@
 (function CharacterTypeScope(mongoose) {
-  'use strict';
+	'use strict';
 
-  var Schema = mongoose.Schema;
+	var Schema = mongoose.Schema;
 
-  var characterTypeSchema = new Schema({
-    _id: Number,
-    name: String
-  });
+	var characterTypeSchema = new Schema({
+		_id: Number,
+		name: String
+	}, {
+		collection: 'characterTypes'
+	});
 
-  var CharacterType = mongoose.model('CharacterType', characterTypeSchema);
-  CharacterType.CharacterTypes = {
-    SOLDIER: 0,
-    LANCER: 1,
-    KNIGHT: 2,
-    ARCHER: 3,
-    MAGE: 4,
-    HERO: 5,
-    SAGE: 6
-  };
+	var CharacterType = mongoose.model('CharacterType', characterTypeSchema);
+	CharacterType.CharacterTypes = {
+		SOLDIER: 0,
+		LANCER: 1,
+		KNIGHT: 2,
+		ARCHER: 3,
+		MAGE: 4,
+		HERO: 5,
+		SAGE: 6
+	};
 
-  module.exports = CharacterType;
+	module.exports = CharacterType;
 }(require('mongoose')));
