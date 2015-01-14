491feaafdc5f4d8650a5576fb61ffdac9288ef9f
Andy Tang <andyt@live.nl>
2015-01-14 07:16:58 +0100

    groc is now executed only during the deploy step

--- scripts/groc.sh
+++ scripts/groc.sh
@@ -1,12 +1,9 @@
-#!/bin/bash 
-if [ $WERCKER_GIT_BRANCH == "master" ];
-  then echo "building groc documentation";
-  git checkout master;
-  grunt groc:dist;
-  rm -rf app/**/**/*.* app/**/*.js app/*.js test .jshintrc bower.json .bowerrc index.js package.json;
-  git config user.name "drone.io CI";
-  git config user.email "andortang+drone.io@gmail.com";
-  git add -A;
-  git commit -m "Automatically updated the groc documentation via drone.io";
-  git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages;
-fi;
+#!/bin/bash
+echo "building groc documentation";
+git checkout -b grocdoc;
+rm -rf app/**/**/*.* app/**/*.js app/*.js test .jshintrc bower.json .bowerrc index.js package.json;
+git config user.name "wercker CI";
+git config user.email "andortang+wercker.com@gmail.com";
+git add -A;
+git commit -m "Automatically updated the groc documentation via wercker.com";
+git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" grocdoc:gh-pages;
