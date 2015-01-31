f5af0a12838003d5c1520902d4a0dee55f2d94a8
Andy Tang <andyt@live.nl>
2015-01-31 08:30:42 +0100

    added user auth with auth token spec

--- test/features/login.feature
+++ test/features/login.feature
@@ -19,3 +19,9 @@ Feature: Login on Imber
       And player provides "" as password
     When player attempts to log in
     Then player should not be logged in
+
+  Scenario: Player logs in with authToken
+    Given player has logged in before
+      And the token is correct
+    When player refreshes the page
+    Then player should be logged in
--- test/step_definitions/login.step.js
+++ test/step_definitions/login.step.js
@@ -7,7 +7,9 @@
     })
     .given('player provides "(.*)" as (.*)', function(value, model) {
       this.ctx.$scope[model.toCamelCase()] = value || null;
-      console.log(this.ctx.$scope[model.toCamelCase()])
+    })
+    .given('player has logged in before', function() {
+      this.ctx.ipCookie('authToken', 'abcxyz');
     })
     .given('the credentials are correct', function() {
       this.ctx.expectedResponse = {
@@ -22,6 +24,19 @@
         password: this.ctx.$scope.password
       }).respond(200, this.ctx.expectedResponse);
     })
+    .given('the token is correct', function() {
+      this.ctx.expectedResponse = {
+        authToken: 'returnedAuthToken',
+        user: {
+          _id: 'a1b2c3d4e5f6g7',
+          userName: this.ctx.$scope.userName
+        }
+      };
+      this.ctx.$httpBackend.expect('POST',
+        '/api/reauthenticate', {
+          authToken: this.ctx.ipCookie('authToken')
+        }).respond(200, this.ctx.expectedResponse);
+    })
     .when('player logs in', function() {
       this.ctx.$scope.login();
       this.ctx.$httpBackend.flush();
@@ -29,6 +44,10 @@
     .when('player attempts to log in', function() {
       sinon.spy(this.ctx.userDAO, 'login');
       this.ctx.$scope.login();
+    })
+    .when('player refreshes the page', function() {
+      this.ctx.$scope.login();
+      this.ctx.$httpBackend.flush();
     })
     .then('player should be logged in', function() {
       expect(this.ctx.$scope.loggedIn()).to.be.true;
